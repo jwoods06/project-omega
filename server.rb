171732775181bc1c3210c20e95b8c77b4c3d16f6
Agent Jones <agentjones@gitlab.com>
2017-12-13 17:18:46 +0000

    Made things wonderful

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ server = WEBrick::HTTPServer.new :Port => 5000
 
 #The following proc is used to customize the server operations
 server.mount_proc '/' do |request, response|
-  response.body = 'Hello World!'
+  response.body = 'Hello Wonderful World!'
 end
 
 server.start
